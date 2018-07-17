ebe19612e1d805c082af8baf54f0b4e2599e1e64
Micah81 <micahmrocks@gmail.com>
2018-07-16 21:16:41 -0600

    init

---
+++ src/store.js
@@ -0,0 +1,59 @@
+{
+  type: ‘ADD_TODO’,
+  todo: {
+
+    id: 0,
+
+    name: ‘Learn Redux’,
+
+    complete: false,
+
+  }
+}
+
+{
+  type: ‘REMOVE_TODO’,
+  id: 0,
+}
+
+{
+  type: ‘TOGGLE_TODO’,
+  id: 0
+}
+
+{
+  type: ‘ADD_GOAL’,
+  goal: {
+    id: 0,
+    name: ‘Run a Marathon’
+  }
+}
+
+{
+  type: ‘REMOVE_GOAL’,
+  id: 0
+}
+
+
+
+function createStore() {
+let state;
+let listeners = []
+const getState = () => state;
+const subscribe = (listener) => {
+  listeners.push(listener)
+  return () {
+    listeners = listeners.filter((l) => l !== listener)
+  }
+}
+
+return {
+  getState,
+  subscribe
+}}
+
+const store = createStore();
+
+store.subscribe(() => {
+  
+})
